e236f1103f1624c82e5382d9f1e4741f6e5dbc43
HanYoonSoo <hdbstn3055@gmail.com>
2023-04-04 16:04:41 +0900

    ExceptionHandle

---
+++ src/main/java/com/mirae/warmup/warmUp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.mirae.warmup.warmUp.exception;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /* 404 오류 입니다. */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFoundPage(NoHandlerFoundException e){
+        System.out.println("NotFoundException 에러 발생");
+        return "error404";
+    }
+
+    /* 데이터 베이스 오류 입니다. */
+    @ExceptionHandler(DataAccessException.class)
+    public String handleDataAccessException(DataAccessException e) {
+        e.printStackTrace();
+        return "error";
+    }
+    /* 서버 오류 입니다. */
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        e.printStackTrace();
+        return "error";
+    }
+}
