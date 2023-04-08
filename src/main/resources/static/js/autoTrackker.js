9e1017409969a5192304667998b34068b95bebab
gunyu1019 <gunyu1019@gmail.com>
2023-04-09 03:09:51 +0900

    [refactor] before merge

--- src/main/resources/static/js/autoTrackker.js
+++ src/main/resources/static/js/autoTrackker.js
@@ -1,5 +1,5 @@
 
-function autoedTracker(groupContent) {
+function autoedTrack(groupContent) {
     const groupWidth = document.querySelector('div.image_group').scrollWidth
     for (let i = 0; i < screen.width * 2; i += groupWidth) {
         document.querySelector('div.image_group').innerHTML += groupContent
