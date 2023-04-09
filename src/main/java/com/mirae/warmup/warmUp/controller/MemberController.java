0dcf11b34f94dfad943f110d18d4ff645eb75514
gunyu1019 <gunyu1019@gmail.com>
2023-04-09 17:04:49 +0900

    [merge] before merge

--- src/main/java/com/mirae/warmup/warmUp/controller/MemberController.java
+++ src/main/java/com/mirae/warmup/warmUp/controller/MemberController.java
@@ -1,16 +1,10 @@
 package com.mirae.warmup.warmUp.controller;
 
 import com.mirae.warmup.warmUp.config.auth.PrincipalDetails;
-import com.mirae.warmup.warmUp.dto.UserDto;
-import com.mirae.warmup.warmUp.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
     http://localhost:8080/swagger-ui.html 로 접속하시면 컨트롤러 시험 운용 가능하십니다.
@@ -19,8 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/mirae")
 public class MemberController {
 
-    @GetMapping({"/team/yong_hyun"})
-    public String home(){
+    @GetMapping({"/user/yong_hyun"})
+    public String yong_hyun(@AuthenticationPrincipal PrincipalDetails principalDetails){
         return "team/yong_hyun";
     }
 }
